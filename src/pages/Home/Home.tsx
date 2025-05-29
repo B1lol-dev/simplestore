f84711499590f21809920dc1f86db14b385f0d32
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-29 22:51:48 +0500

    remove unneccecary imports

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,7 +8,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { ShoppingBag, Users } from "lucide-react";
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
